aab5f8856b1614253269f760bcb9b43ce780db89
josepaniagua <jpanjr@gmail.com>
2020-02-19 19:50:37 -0800

    setdarkvalue

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,5 +1,12 @@
+import { useEffect } from 'react';
 import { useLocalStorage } from "./useLocalStorage";
 
-export const useDarkMode = () => {
-    const [darkValue, setDarkValue] = useLocalStorage('your key here')
-}
+export const useDarkMode = (dark1) => {
+    const [darkValue, setDarkValue] = useLocalStorage('bienOscuro', dark1);
+    useEffect(() => {
+        const body = document.querySelector("body");
+        darkValue ? body.classList.add("dark-mode") : body.classList.remove("dark-mode")
+    }, [darkValue])
+
+    return [darkValue, setDarkValue];
+};
